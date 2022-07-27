39544af2b638543d43ce5c78ce71f3bfaa2d9dbd
Mona Elshikh <mona.elshikh@hotmail.com>
2022-07-27 20:44:33 +0200

    pahse two desktop design and finishing js code

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 
 //#region  Declaration
-const url = "../data/data.json";
+// const url = "../data/data.json";
+const url = "https://github.com/MonaElshikh/Time-tracking-dashboard/data/data.json";
+
+let trackContainer = document.querySelector(".time-track");
+let tracksPeriods = document.querySelectorAll(".dashboard > ul >li >a");
 const imgsSrc = [
     { Work: "../images/icon-work.svg" },
     { Play: "../images/icon-play.svg" },
     { Study: "../images/icon-study.svg" },
     { Exercise: "../images/icon-Exercise.svg" },
     { Social: "../images/icon-Social.svg" },
-    { SelfCare: "../images/icon-self-care.svg" }]
-let trackContainer = document.querySelector(".time-track");
-console.log(trackContainer);
+    { SelfCare: "../images/icon-self-care.svg" }];
 //#endregion
 
 //#region  Functions
@@ -17,21 +19,20 @@ console.log(trackContainer);
 async function getData() {
     let result = await fetch(url);
     let data = await result.json();
-    createTimeTrackingBoxes(data);
-    console.log(data);
-}
 
-// function to create tracking
-function createTimeTrackingBoxes(tracks) {
+    createTimeTracks(data);
+    filterTimeTracks(data, tracksPeriods);
+}
+// function to create time tracks boxes
+function createTimeTracks(tracks) {
     tracks.forEach((track, index) => {
-        // console.log(imgsSrc[index][track.title]);
         // create trackBox div
         let trackBoxDiv = document.createElement("div");
         trackBoxDiv.className = "track-box";
 
         // create colored header and image
         let coloredHeadDiv = document.createElement("div");
-        coloredHeadDiv.className = "colored-head ";
+        coloredHeadDiv.className = "track-header";
         coloredHeadDiv.classList.add(`${track.title.replace(" ", "").toLowerCase()}-color`);
         let trackImg = document.createElement("img");
         trackImg.src = imgsSrc[index][track.title.replace(" ", "")];
@@ -39,9 +40,25 @@ function createTimeTrackingBoxes(tracks) {
 
         // add trackImg to colored head
         coloredHeadDiv.appendChild(trackImg);
-
         // adding colored header to trackbox
         trackBoxDiv.appendChild(coloredHeadDiv);
+
+        // create trackContnent div
+        let trackContentDiv = document.createElement("div");
+        trackContentDiv.className = "track-content";
+
+        // create track title div
+        let trackTitleDiv = document.createElement("div");
+        trackTitleDiv.className = "track-title";
+        let titleDiv = document.createElement("h2");
+        titleDiv.innerHTML = track.title;
+        trackTitleDiv.appendChild(titleDiv);
+        let prevImg = document.createElement("img");
+        prevImg.src = "../images/icon-ellipsis.svg";
+        prevImg.alt = "icon-ellipsis";
+        trackTitleDiv.appendChild(prevImg);
+        // add track title to trackContent div
+        trackContentDiv.appendChild(trackTitleDiv);
 
         // create trackInfo div
         let trackInfoDiv = document.createElement("div");
@@ -50,39 +67,53 @@ function createTimeTrackingBoxes(tracks) {
         //#region  current div
         let currentDiv = document.createElement("div");
         currentDiv.className = "current";
-
-        let titleDiv = document.createElement("h2");
-        titleDiv.innerHTML = track.title;
-        currentDiv.appendChild(titleDiv);
-
         let hrsDiv = document.createElement("h3");
-        hrsDiv.innerHTML = "23hrs";
+        hrsDiv.innerHTML = `${track.timeframes.weekly.current}hr(s)`;
         currentDiv.appendChild(hrsDiv);
-
         trackInfoDiv.appendChild(currentDiv);
         //#endregion
 
         //#region  previous div
         let previousDiv = document.createElement("div");
         previousDiv.className = "previous";
-
-        let prevImg = document.createElement("img");
-        prevImg.src = "../images/icon-ellipsis.svg";
-        prevImg.alt = "icon-ellipsis";
-        previousDiv.appendChild(prevImg);
-
         let prevTxt = document.createElement("p");
-        prevTxt.innerHTML = "Last 20hrs";
+        prevTxt.innerHTML = `Last Week - ${track.timeframes.weekly.previous}hr(s)`;
         previousDiv.appendChild(prevTxt);
-
         trackInfoDiv.appendChild(previousDiv);
         //#endregion
+        // add track info to trackContnent
+        trackContentDiv.appendChild(trackInfoDiv);
 
         // appedn track info div to track box div
-        trackBoxDiv.appendChild(trackInfoDiv);
+        trackBoxDiv.appendChild(trackContentDiv);
         // adding trackbox div to track container div
         trackContainer.appendChild(trackBoxDiv);
+    });
 
+}
+// function to filetr tracks according to period clicked by user.
+function filterTimeTracks(tracks, periods) {
+    let currentPeriod; //clicked link value
+    let currents = document.querySelectorAll(".current > h3");
+    let previous = document.querySelectorAll(".previous > p");
+    periods.forEach((period) => {
+        period.addEventListener("click", () => {
+            // clrear active style from all periods links
+            period.parentElement.parentElement.querySelectorAll("li> a").forEach((item) => item.classList.remove("active"));
+            // set current link to active
+            period.classList.add("active");
+            // set previous text according to period link clicked.
+            period.innerHTML === "Daily"
+                ? currentPeriod = "Day"
+                : period.innerHTML === "Weekly"
+                    ? currentPeriod = "Week" :
+                    currentPeriod = "Month";
+            // set currents and previous values from json data.
+            tracks.forEach((track, index) => {
+                currents[index].innerHTML = `${track.timeframes[period.innerHTML.toLowerCase()].current}hr(s)`;
+                previous[index].innerHTML = ` Last ${currentPeriod} - ${track.timeframes[period.innerHTML.toLowerCase()].previous}hr(s)`;
+            });
+        });
     });
 }
 //#endregion
