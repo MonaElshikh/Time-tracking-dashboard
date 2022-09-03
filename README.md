dc0c28cd417b59ebea62a8606b1cff0439addec1
Mona Elshikh <mona.elshikh@hotmail.com>
2022-09-03 22:52:10 +0200

    refact code - using typescript and update readme file adding screenshots

--- README.md
+++ README.md
@@ -1,20 +1,57 @@
 # Frontend Mentor - Time tracking dashboard solution
 
-# The challenge
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+Users should be able to:
 
 - View the optimal layout for the site depending on their device's screen size
 - See hover states for all interactive elements on the page
 - Switch between viewing Daily, Weekly, and Monthly stats
 
-# Built with
+### Screenshot
+
+![](./screenshots/mobile.png)
+![](./screenshots/desktop.png)
+
+### Links
+
+- Solution URL: [https://github.com/MonaElshikh/Time-tracking-dashboard]
+- Live Site URL: [https://monaelshikh.github.io/Time-tracking-dashboard/]
+
+## My process
+
+### Built with
 
 - Semantic HTML5 markup
 - CSS custom properties
 - Flexbox
-- Javascript
-- CSS Grid
 - Mobile-first workflow
+- Typescript
 
-# Acknowledgments
+## Author
 
-- Thanks a lot Frontendmentor for your help and support :)
+- Frontend Mentor - [@MonaElshikh](https://www.frontendmentor.io/profile/MonaElshikh)
+
+## Acknowledgments
+
+Thanks Frontendmentor for your support :)
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
+
+//#region  Declaration
+import { TimeTrack } from "./time-track";
 const url = "https://monaelshikh.github.io/Time-tracking-dashboard/data/data.json";
-let trackContainer = document.querySelector(".time-track");
+let trackContainer = document.querySelector(".time-track") as HTMLDivElement;
 let tracksPeriods = document.querySelectorAll(".dashboard > ul >li >a");
 const imgsSrc = [
     { Work: "images/icon-work.svg" },
@@ -16,30 +10,42 @@ const imgsSrc = [
     { Study: "images/icon-study.svg" },
     { Exercise: "images/icon-exercise.svg" },
     { Social: "images/icon-social.svg" },
-    { SelfCare: "images/icon-self-care.svg" }
-];
-function getData() {
-    return __awaiter(this, void 0, void 0, function* () {
-        let result = yield fetch(url);
-        let data = yield result.json();
-        createTimeTracks(data);
-        filterTimeTracks(data, tracksPeriods);
-    });
+    { SelfCare: "images/icon-self-care.svg" }];
+//#endregion
+//#region  Functions
+// function to get data from json file
+async function getData() {
+    let result = await fetch(url);
+    let data = await result.json();
+
+    createTimeTracks(data);
+    filterTimeTracks(data, tracksPeriods);
 }
-function createTimeTracks(tracks) {
-    tracks.forEach((track, index) => {
+// function to create time tracks boxes
+function createTimeTracks(tracks:TimeTrack[]) {
+    tracks.forEach((track:TimeTrack, index:number) => {
+        // create trackBox div
         let trackBoxDiv = document.createElement("div");
         trackBoxDiv.className = "track-box";
+
+        // create colored header and image
         let coloredHeadDiv = document.createElement("div");
         coloredHeadDiv.className = "track-header";
         coloredHeadDiv.classList.add(`${track.title.replace(" ", "").toLowerCase()}-color`);
         let trackImg = document.createElement("img");
         trackImg.src = imgsSrc[index][track.title.replace(" ", "")];
         trackImg.alt = track.title;
+
+        // add trackImg to colored head
         coloredHeadDiv.appendChild(trackImg);
+        // adding colored header to trackbox
         trackBoxDiv.appendChild(coloredHeadDiv);
+
+        // create trackContnent div
         let trackContentDiv = document.createElement("div");
         trackContentDiv.className = "track-content";
+
+        // create track title div
         let trackTitleDiv = document.createElement("div");
         trackTitleDiv.className = "track-title";
         let titleDiv = document.createElement("h2");
@@ -49,39 +55,58 @@ function createTimeTracks(tracks) {
         prevImg.src = "images/icon-ellipsis.svg";
         prevImg.alt = "icon-ellipsis";
         trackTitleDiv.appendChild(prevImg);
+        // add track title to trackContent div
         trackContentDiv.appendChild(trackTitleDiv);
+
+        // create trackInfo div
         let trackInfoDiv = document.createElement("div");
         trackInfoDiv.className = "track-info";
+
+        //#region  current div
         let currentDiv = document.createElement("div");
         currentDiv.className = "current";
         let hrsDiv = document.createElement("h3");
         hrsDiv.innerHTML = `${track.timeframes.weekly.current}hr(s)`;
         currentDiv.appendChild(hrsDiv);
         trackInfoDiv.appendChild(currentDiv);
+        //#endregion
+
+        //#region  previous div
         let previousDiv = document.createElement("div");
         previousDiv.className = "previous";
         let prevTxt = document.createElement("p");
         prevTxt.innerHTML = `Last Week - ${track.timeframes.weekly.previous}hr(s)`;
         previousDiv.appendChild(prevTxt);
         trackInfoDiv.appendChild(previousDiv);
+        //#endregion
+        // add track info to trackContnent
         trackContentDiv.appendChild(trackInfoDiv);
+
+        // appedn track info div to track box div
         trackBoxDiv.appendChild(trackContentDiv);
+        // adding trackbox div to track container div
         trackContainer.appendChild(trackBoxDiv);
     });
+
 }
-function filterTimeTracks(tracks, periods) {
-    let currentPeriod;
+// function to filetr tracks according to period clicked by user.
+function filterTimeTracks(tracks: TimeTrack[], periods: any[] | NodeListOf<Element>): void {
+    let currentPeriod; //clicked link value
     let currents = document.querySelectorAll(".current > h3");
     let previous = document.querySelectorAll(".previous > p");
     periods.forEach((period) => {
         period.addEventListener("click", () => {
+            // clrear active style from all periods links
             period.parentElement.parentElement.querySelectorAll("li> a").forEach((item) => item.classList.remove("active"));
+            // set current link to active
             period.classList.add("active");
+            // set previous text according to period link clicked.
             period.innerHTML === "Daily"
                 ? currentPeriod = "Yesterday"
                 : period.innerHTML === "Weekly"
                     ? currentPeriod = "Last Week" :
                     currentPeriod = "Last Month";
+            // set currents and previous values from json data.
             tracks.forEach((track, index) => {
                 currents[index].innerHTML = `${track.timeframes[period.innerHTML.toLowerCase()].current}hr(s)`;
                 previous[index].innerHTML = `${currentPeriod} - ${track.timeframes[period.innerHTML.toLowerCase()].previous}hr(s)`;
@@ -89,5 +114,7 @@ function filterTimeTracks(tracks, periods) {
         });
     });
 }
+//#endregion
+//#region  Calls
 getData();
-export {};
+//#endregion
